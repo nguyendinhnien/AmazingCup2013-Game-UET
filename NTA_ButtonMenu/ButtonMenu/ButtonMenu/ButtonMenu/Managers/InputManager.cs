c6600c2e1eb3d12a96da176bde163ffd934370f5
Tuan Anh Nguyen <itisok2bm@gmail.com>
2013-03-01 16:58:00 +0700

    Add Project Button Menu

---
+++ NTA_ButtonMenu/ButtonMenu/ButtonMenu/ButtonMenu/Managers/InputManager.cs
@@ -0,0 +1,86 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// InputManager.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace ButtonMenu
+{
+    /// <summary>
+    /// This class handles all mouse actions in the game.
+    /// </summary>
+    public static class InputManager
+    {
+        private static MouseState currentMouseState;
+        private static bool currentMousePressed;
+        private static bool previousMousePressed;
+
+        /// <summary>
+        /// Check if the mouse is down.
+        /// </summary>
+        public static bool IsMouseDown()
+        {
+            return currentMousePressed;
+        }
+
+        /// <summary>
+        /// check if the mouse is triggered
+        /// </summary>
+        public static bool IsMouseTriggered()
+        {
+            return !previousMousePressed && currentMousePressed;
+        }
+
+        /// <summary>
+        /// Check if the mouse is just released
+        /// </summary>
+        public static bool IsMouseJustReleased()
+        {
+            return previousMousePressed && !currentMousePressed;
+        }
+
+        /// <summary>
+        /// determine if the mouse hitted the rectangle
+        /// </summary>
+        public static Boolean IsMouseHittedRectangle(Rectangle rect)
+        {
+            float x = currentMouseState.X;
+            float y = currentMouseState.Y;
+            return (x >= rect.Left &&
+                x <= rect.Right &&
+                y >= rect.Top &&
+                y <= rect.Bottom);
+
+        }
+
+        /// <summary>
+        /// Initializes the default control keys for all actions.
+        /// </summary>
+        public static void Initialize()
+        {
+        }
+
+        /// <summary>
+        /// Updates the keyboard and gamepad control states.
+        /// </summary>
+        public static void Update()
+        {
+            // update the mouse state
+            previousMousePressed = currentMousePressed;
+            currentMouseState = Mouse.GetState();
+            currentMousePressed =
+                (currentMouseState.LeftButton == ButtonState.Pressed);
+        }
+    }
+}

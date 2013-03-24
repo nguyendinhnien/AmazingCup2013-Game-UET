2f7848eee32c8781c0aeec008de30d588d929bbb
Tuan Anh Nguyen <itisok2bm@gmail.com>
2013-03-24 10:26:57 +0700

    update images

--- ForestGuardian/Game/Game/Scene/HelpScene.cs
+++ ForestGuardian/Game/Game/Scene/HelpScene.cs
@@ -14,6 +14,10 @@ namespace CustomGame
 {
     public class HelpScene : GameScene
     {
+        private static int NUMBER_OF_ITEM_DISPLAY = 3;
+
+        private static Vector2[] itemPosition;
+
         private Button backButton;
         private Texture2D backgroundTexture;
         private Vector2 backgroundPosition;
@@ -24,15 +28,24 @@ namespace CustomGame
         private int numberOfItems;
         private Texture2D[] itemTexture;
 
-        private int currentItem;
+        private Texture2D hightlightSelectTexture;
+        private SpriteFont helpFont;
+
+        private int startItem;
+        private int currentItemShow;
 
         public HelpScene()
             : base()
         {
             numberOfItems = 5;
             itemTexture = new Texture2D[numberOfItems];
+            itemPosition = new Vector2[NUMBER_OF_ITEM_DISPLAY];
 
-            currentItem = 0;
+            itemPosition[0] = new Vector2(190, 467);
+            itemPosition[1] = itemPosition[0] + new Vector2(230, 0);
+            itemPosition[2] = itemPosition[1] + new Vector2(230, 0);
+
+            startItem = currentItemShow = 0;
         }
 
         public override void LoadContent()
@@ -63,6 +76,9 @@ namespace CustomGame
             itemTexture[2] = content.Load<Texture2D>(@"images\scene\HelpScene\pine_apple_card");
             itemTexture[3] = content.Load<Texture2D>(@"images\scene\HelpScene\pine_apple_card");
             itemTexture[4] = content.Load<Texture2D>(@"images\scene\HelpScene\pine_apple_card");
+
+            hightlightSelectTexture = content.Load<Texture2D>(@"images\scene\HelpScene\highlight_select_card");
+            helpFont = content.Load<SpriteFont>(@"fonts\HelpScene\helpscene");
         }
 
         public override void Update(GameTime gameTime)
@@ -71,15 +87,25 @@ namespace CustomGame
             backwardButton.Update(gameTime);
             forwardButton.Update(gameTime);
 
-            if (currentItem == 0)
+            if (startItem == 0)
                 backwardButton.Active = false;
             else
                 backwardButton.Active = true;
 
-            if (currentItem == numberOfItems - 3)
+            if (startItem == numberOfItems - NUMBER_OF_ITEM_DISPLAY)
                 forwardButton.Active = false;
             else
                 forwardButton.Active = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (InputManager.IsMouseJustReleased() && InputManager.IsMouseHittedRectangle(new Rectangle(
+                    (int)itemPosition[i].X, (int)itemPosition[i].Y,
+                    itemTexture[startItem + i].Width, itemTexture[startItem + i].Height)))
+                {
+                    currentItemShow = i;
+                }
+            }
         }
 
 
@@ -92,24 +118,27 @@ namespace CustomGame
             backwardButton.Draw(spriteBatch);
             forwardButton.Draw(spriteBatch);
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < NUMBER_OF_ITEM_DISPLAY; i++)
             {
-                spriteBatch.Draw(itemTexture[currentItem + i], new Vector2(190, 467)
-                    + i * new Vector2(230, 0), Color.White);
+                spriteBatch.Draw(itemTexture[startItem + i], itemPosition[i], Color.White);
             }
+
+            spriteBatch.Draw(hightlightSelectTexture, itemPosition[currentItemShow], Color.White);
+            spriteBatch.DrawString(helpFont, "con meo con. anh tuan anh", new Vector2(500, 100), Color.White);
+
             spriteBatch.End();
         }
 
         private void BackwardButtonClicked(object sender, EventArgs e)
         {
-            if (currentItem > 0)
-                currentItem--;
+            if (startItem > 0)
+                startItem--;
         }
 
         private void ForwardButtonClicked(object sender, EventArgs e)
         {
-            if (currentItem < numberOfItems - 3)
-                currentItem++;
+            if (startItem < numberOfItems - NUMBER_OF_ITEM_DISPLAY)
+                startItem++;
         }
 
         private void BackButtonClicked(object sender, EventArgs e)
--- ForestGuardian/Game/Game/Scene/SelectLevelScene.cs
+++ ForestGuardian/Game/Game/Scene/SelectLevelScene.cs
@@ -14,24 +14,157 @@ namespace CustomGame
 {
     public class SelectLevelScene : GameScene
     {
+        private static int NUMBER_OF_ITEM_DISPLAY = 3;
+
+        private static Vector2[] itemPosition;
+
+        private Button backButton;
+        private Button startButton;
+
+        private Texture2D backgroundTexture;
+        private Vector2 backgroundPosition;
+
+        private ToggleButton backwardButton;
+        private ToggleButton forwardButton;
+
+        private int numberOfItems;
+        private Texture2D[] itemTexture;
+
+        private Texture2D hightlightSelectTexture;
+        private Texture2D[] mapPreviewTexture;
+
+        private SpriteFont helpFont;
+
+        private int startItem;
+        private int currentItemShow;
 
         public SelectLevelScene()
             : base()
         {
+            numberOfItems = 5;
+            itemTexture = new Texture2D[numberOfItems];
+            itemPosition = new Vector2[NUMBER_OF_ITEM_DISPLAY];
+            mapPreviewTexture = new Texture2D[numberOfItems];
+
+            itemPosition[0] = new Vector2(190, 467);
+            itemPosition[1] = itemPosition[0] + new Vector2(230, 0);
+            itemPosition[2] = itemPosition[1] + new Vector2(230, 0);
+
+            startItem = currentItemShow = 0;
         }
 
         public override void LoadContent()
         {
+            ContentManager content = SceneManager.Game.Content;
+            backgroundTexture = content.Load<Texture2D>(@"images\scene\SelectLevelScene\selectmaps");
+            backgroundPosition = new Vector2(0, 0);
 
+            Texture2D texture = content.Load<Texture2D>(@"images\scene\HelpScene\b_back");
+            Texture2D pressTexture = content.Load<Texture2D>(@"images\scene\HelpScene\b_back_clicked");
+            backButton = new Button(texture, null, pressTexture, new Vector2(500, 690));
+            backButton.Clicked += BackButtonClicked;
+
+            texture = content.Load<Texture2D>(@"images\scene\SelectLevelScene\b_start_game");
+            pressTexture = content.Load<Texture2D>(@"images\scene\SelectLevelScene\b_start_game_clicked");
+            startButton = new Button(texture, null, pressTexture, new Vector2(300, 690));
+            startButton.Clicked += StartButtonClicked;
+
+            Texture2D disableButton = content.Load<Texture2D>(@"images\scene\HelpScene\b_backward_disable");
+            texture = content.Load<Texture2D>(@"images\scene\HelpScene\b_backward");
+            pressTexture = content.Load<Texture2D>(@"images\scene\HelpScene\b_backward_clicked");
+            backwardButton = new ToggleButton(texture, null, pressTexture, disableButton, new Vector2(50, 510));
+            backwardButton.Clicked += BackwardButtonClicked;
+
+            disableButton = content.Load<Texture2D>(@"images\scene\HelpScene\b_forward_disable");
+            texture = content.Load<Texture2D>(@"images\scene\HelpScene\b_forward");
+            pressTexture = content.Load<Texture2D>(@"images\scene\HelpScene\b_forward_clicked");
+            forwardButton = new ToggleButton(texture, null, pressTexture, disableButton, new Vector2(900, 510));
+            forwardButton.Clicked += ForwardButtonClicked;
+
+            itemTexture[0] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_1_thumb");
+            itemTexture[1] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_2_thumb");
+            itemTexture[2] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_3_thumb");
+            itemTexture[3] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_2_thumb");
+            itemTexture[4] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_1_thumb");
+
+            mapPreviewTexture[0] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_1_preview");
+            mapPreviewTexture[1] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_2_preview");
+            mapPreviewTexture[2] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_3_preview");
+            mapPreviewTexture[3] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_2_preview");
+            mapPreviewTexture[4] = content.Load<Texture2D>(@"images\scene\SelectLevelScene\map_level_1_preview");
+
+            hightlightSelectTexture = content.Load<Texture2D>(@"images\scene\HelpScene\highlight_select_card");
+            helpFont = content.Load<SpriteFont>(@"fonts\HelpScene\helpscene");
         }
 
         public override void Update(GameTime gameTime)
         {
+            backButton.Update(gameTime);
+            backwardButton.Update(gameTime);
+            forwardButton.Update(gameTime);
+
+            if (startItem == 0)
+                backwardButton.Active = false;
+            else
+                backwardButton.Active = true;
+
+            if (startItem == numberOfItems - NUMBER_OF_ITEM_DISPLAY)
+                forwardButton.Active = false;
+            else
+                forwardButton.Active = true;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (InputManager.IsMouseJustReleased() && InputManager.IsMouseHittedRectangle(new Rectangle(
+                    (int)itemPosition[i].X, (int)itemPosition[i].Y,
+                    itemTexture[startItem + i].Width, itemTexture[startItem + i].Height)))
+                {
+                    currentItemShow = i;
+                }
+            }
         }
+
 
         public override void Draw(SpriteBatch spriteBatch)
         {
+            spriteBatch.Begin();
 
+            spriteBatch.Draw(backgroundTexture, backgroundPosition, Color.White);
+            backButton.Draw(spriteBatch);
+            backwardButton.Draw(spriteBatch);
+            forwardButton.Draw(spriteBatch);
+
+            for (int i = 0; i < NUMBER_OF_ITEM_DISPLAY; i++)
+            {
+                spriteBatch.Draw(itemTexture[startItem + i], itemPosition[i], Color.White);
+            }
+
+            spriteBatch.Draw(hightlightSelectTexture, itemPosition[currentItemShow], Color.White);
+            spriteBatch.Draw(mapPreviewTexture[currentItemShow], new Vector2(40, 103), Color.White);
+
+            spriteBatch.End();
+        }
+
+        private void StartButtonClicked(object sender, EventArgs e)
+        {
+            SceneManager.AddScene(new GamePlayScene());
+        }
+
+        private void BackwardButtonClicked(object sender, EventArgs e)
+        {
+            if (startItem > 0)
+                startItem--;
+        }
+
+        private void ForwardButtonClicked(object sender, EventArgs e)
+        {
+            if (startItem < numberOfItems - NUMBER_OF_ITEM_DISPLAY)
+                startItem++;
+        }
+
+        private void BackButtonClicked(object sender, EventArgs e)
+        {
+            this.ExitScreen();
         }
     }
 }
